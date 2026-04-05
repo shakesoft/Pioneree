61acb9d27155c68d8d46ce35a465fc2c853597b5
metasharp <1176758572@qq.com>
2026-04-05 08:58:57 +0800

    clean https

--- aspnet-core/src/PionereeDemo.Web.Host/Startup/PionereeDemoWebHostModule.cs
+++ aspnet-core/src/PionereeDemo.Web.Host/Startup/PionereeDemoWebHostModule.cs
@@ -44,7 +44,7 @@ public class PionereeDemoWebHostModule : AbpModule
     public override void PreInitialize()
     {
         Configuration.Modules.AbpWebCommon().MultiTenancy.DomainFormat =
-            _appConfiguration["App:ServerRootAddress"] ?? "https://localhost:44301/";
+            _appConfiguration["App:ServerRootAddress"] ?? "http://localhost:44301/";
         Configuration.Modules.AspNetZero().LicenseCode = _appConfiguration["AbpZeroLicenseCode"];
     }
 
